20dc405ff0361fb983ada5d942c5ec51664ef76d
fabioadelfio <adelfiofabio07@gmail.com>
2025-06-17 18:08:28 +0200

    CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const moviesRoutes = require('./routes/movies');
-const notFound = require('./middleware/notFound');
-const errorHandler = require('./middleware/errorHandler');
+const notFound = require(`./middlewares/notFound`);
+const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 
 // Global Middlewares
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
